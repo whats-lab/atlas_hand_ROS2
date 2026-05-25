7411c4c8d9ae35d56a49122ea7ad22bae16b40ba
MINJUN.PARK <MINJUN.PARK@whatslab.co.kr>
2026-05-26 08:52:44 +0900

    chore : docker 내용 수정

--- docker/docker.sh
+++ docker/docker.sh
@@ -14,8 +14,8 @@ FASTDDS_TRANSPORT="${FASTDDS_BUILTIN_TRANSPORTS:-UDPv4}"
 case "$1" in
 
   build)
-    echo ">>> 이미지 빌드"
-    docker build -f "${SCRIPT_DIR}/Dockerfile" \
+    echo ">>> 이미지 빌드 (--no-cache)"
+    docker build --no-cache -f "${SCRIPT_DIR}/Dockerfile" \
       -t "$IMAGE" "$REPO_ROOT"
     ;;
 
